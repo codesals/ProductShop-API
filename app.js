00e925262aba272f4a6e7bdf5e70b0e6debfdd31
Sally <78157471+codesals@users.noreply.github.com>
2021-03-09 19:40:53 +0300

    Finish Product Create route (Express Task 3)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 let products = require("./products");
 const cors = require("cors");
+const slugify = require("slugify");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json()); //replacing body parser
 
 app.get("/", (request, response) => {
   response.json({
@@ -18,13 +20,21 @@ app.get("/products", (request, response) => {
 });
 
 app.delete("/products/:productID", (request, response) => {
-  const { productID } = request.params;
+  const { productID } = request.params; // same as const productID  = request.params.productID;
   const foundProduct = products.find((product) => product.id === +productID);
   if (foundProduct) {
     products = products.filter((product) => product.id !== +foundProduct.id);
     response.status(204).end();
   } else response.status(404).json({ message: "Product not found!" });
   // response.json(products);
+});
+
+app.post("/products", (request, response) => {
+  const id = products[products.length - 1].id + 1;
+  const slug = slugify(request.body.name, { lower: true });
+  const newProduct = { id, slug, ...request.body };
+  products.push(newProduct);
+  response.status(201).json(newProduct);
 });
 
 app.listen(8000, () => {
