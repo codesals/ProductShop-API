5ff030d1349aa8c1421213210c6cf5f8d288c288
Sally <78157471+codesals@users.noreply.github.com>
2021-03-18 15:14:24 +0300

    Add constraints &validations (Express Task 6 done)

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,9 +4,6 @@ const slugify = require("slugify");
 const { Product } = require("../db/models");
 // const { response } = require("express"); //appeared automatically
 
-//data.js list
-// exports.productList = (_, response) => response.json(products);
-
 //db List
 exports.productList = async (_, response) => {
   try {
@@ -20,15 +17,6 @@ exports.productList = async (_, response) => {
   }
 };
 
-// data.js Create
-// exports.productCreate = (request, response) => {
-//   const id = products[products.length - 1].id + 1;
-//   const slug = slugify(request.body.name, { lower: true });
-//   const newProduct = { id, slug, ...request.body };
-//   products.push(newProduct);
-//   response.status(201).json(newProduct);
-// };
-
 //db Create
 exports.productCreate = async (request, response) => {
   try {
@@ -38,16 +26,6 @@ exports.productCreate = async (request, response) => {
     response.status(500).json({ message: error.message });
   }
 };
-
-// data.js Update
-// exports.productUpdate = (request, response) => {
-//   const { productID } = request.params;
-//   const foundProduct = products.find((product) => product.id === +productID);
-//   if (foundProduct) {
-//     for (const key in request.body) foundProduct[key] = request.body[key];
-//     response.status(204).end();
-//   } else response.status(404).json({ message: "Product not found!" });
-// };
 
 //db Update
 exports.productUpdate = async (request, response) => {
@@ -63,12 +41,60 @@ exports.productUpdate = async (request, response) => {
   }
 };
 
-exports.productDelete = (request, response) => {
-  const { productID } = request.params; // same as const productID  = request.params.productID;
-  const foundProduct = products.find((product) => product.id === +productID);
-  if (foundProduct) {
-    products = products.filter((product) => product.id !== +foundProduct.id);
-    response.status(204).end();
-  } else response.status(404).json({ message: "Product not found!" });
-  // response.json(products);
+//db Delete
+exports.productDelete = async (request, response) => {
+  const { productID } = request.params;
+  try {
+    const foundProduct = await Product.findByPk(productID);
+    if (foundProduct) {
+      await foundProduct.destroy();
+      response.status(204).end();
+    } else response.status(404).json({ message: "Product not found!" });
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
 };
+
+// exports.productDelete = (request, response) => {
+//   const { productID } = request.params; // same as const productID  = request.params.productID;
+//   const foundProduct = products.find((product) => product.id === +productID);
+//   if (foundProduct) {
+//     products = products.filter((product) => product.id !== +foundProduct.id);
+//     response.status(204).end();
+//   } else response.status(404).json({ message: "Product not found!" });
+//   // response.json(products);
+// };
+
+//data.js List
+// exports.productList = (_, response) => response.json(products);
+
+// data.js Create
+// exports.productCreate = (request, response) => {
+//   const id = products[products.length - 1].id + 1;
+//   const slug = slugify(request.body.name, { lower: true });
+//   const newProduct = { id, slug, ...request.body };
+//   products.push(newProduct);
+//   response.status(201).json(newProduct);
+// };
+
+// data.js Update
+// exports.productUpdate = (request, response) => {
+//   const { productID } = request.params;
+//   const foundProduct = products.find((product) => product.id === +productID);
+//   if (foundProduct) {
+//     for (const key in request.body) foundProduct[key] = request.body[key];
+//     response.status(204).end();
+//   } else response.status(404).json({ message: "Product not found!" });
+// };
+
+//data.js Delete
+
+// exports.productDelete = (request, response) => {
+//   const { productID } = request.params; // same as const productID  = request.params.productID;
+//   const foundProduct = products.find((product) => product.id === +productID);
+//   if (foundProduct) {
+//     products = products.filter((product) => product.id !== +foundProduct.id);
+//     response.status(204).end();
+//   } else response.status(404).json({ message: "Product not found!" });
+//   // response.json(products);
+// };
--- db/models/Product.js
+++ db/models/Product.js
@@ -6,23 +6,34 @@ module.exports = (sequelize, DataTypes) => {
   const Product = sequelize.define("Product", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     slug: {
       type: DataTypes.STRING,
       unique: true,
     },
-    // testAddColumn: {
-    //   type: DataTypes.STRING,
-    // },
     description: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 5,
+      validate: {
+        min: 1,
+      },
     },
     image: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
     },
+    // testAddColumn: {
+    //   type: DataTypes.STRING,
+    // },
   });
   SequelizeSlugify.slugifyModel(Product, {
     source: ["name"],
