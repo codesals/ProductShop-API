2a4399b42d39bac74af78f3f08e01707dc41f2a3
Sally <78157471+codesals@users.noreply.github.com>
2021-03-18 05:05:20 +0300

    Add DB product list (Express Task 5 Completed)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,25 @@
 let products = require("../data");
 const slugify = require("slugify");
 
+const { Product } = require("../db/models");
+// const { response } = require("express"); //appeared automatically
+
+//old list
+// exports.productList = (_, response) => response.json(products);
+
+//db List
+exports.productList = async (_, response) => {
+  try {
+    const products = await Product.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
+    console.log("products", products);
+    response.json(products);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+};
+
 exports.productCreate = (request, response) => {
   const id = products[products.length - 1].id + 1;
   const slug = slugify(request.body.name, { lower: true });
@@ -8,8 +27,6 @@ exports.productCreate = (request, response) => {
   products.push(newProduct);
   response.status(201).json(newProduct);
 };
-
-exports.productList = (_, response) => response.json(products);
 
 exports.productUpdate = (request, response) => {
   const { productID } = request.params;
